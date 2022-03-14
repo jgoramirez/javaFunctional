64b95f26a8d27510562afe52e8a98d860daa61db
Jose Guadalupe Omar Ramirez Mata <omarin92@gmail.com>
2022-03-13 22:52:09 -0600

    Function
    Programación de Funciones

---
+++ src/main/java/functionalInterface/_Function.java
@@ -0,0 +1,32 @@
+package functionalInterface;
+
+import java.util.function.Function;
+
+public class _Function {
+
+    public static void main(String[] args) {
+        int increment = increment(2);
+        System.out.println(increment);
+
+        int increment2 = incrementByOneFunction.apply(2);
+        System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+
+        System.out.println(addBy1AndThenMultiplyBy10.apply(4));
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number+1;
+
+    static Function<Integer, Integer> multiplyBy10Function =
+            number -> number*10;
+
+    static int increment(int number) {
+        return number + 1;
+    }
+}
