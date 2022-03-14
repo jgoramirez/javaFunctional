e64cc09ab0fe8f75d645c6044bf38aa35ffc56fb
Jose Guadalupe Omar Ramirez Mata <omarin92@gmail.com>
2022-03-13 22:50:44 -0600

    Programacion Imperativa y Declarativa

---
+++ src/main/java/imperative/Main.java
@@ -0,0 +1,65 @@
+package imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static imperative.Main.Gender.FEMALE;
+import static imperative.Main.Gender.MALE;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("Omar", MALE),
+                new Person("John", MALE),
+                new Person("Alissa", FEMALE),
+                new Person("Maria", FEMALE),
+                new Person("Alice", FEMALE)
+        );
+
+        System.out.println("//imperative approach");
+
+        List<Person> females = new ArrayList<>();
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female);
+        }
+
+        System.out.println("//Declarative approach");
+        List<Person> females2 = people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .collect(Collectors.toList());
+        females2.forEach(System.out::println);
+
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        FEMALE,
+        MALE
+    }
+
+}
