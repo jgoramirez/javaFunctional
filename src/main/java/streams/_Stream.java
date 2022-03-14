0396a83c9decb92a5886676b997664b12aee6c43
Jose Guadalupe Omar Ramirez Mata <omarin92@gmail.com>
2022-03-14 01:00:35 -0600

    Ejemplos de uso de java streams

---
+++ src/main/java/streams/_Stream.java
@@ -0,0 +1,68 @@
+package streams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static streams._Stream.Gender.*;
+
+public class _Stream {
+
+    public static void main(String[] args) {
+
+        List<Person> people = List.of(
+                new Person("Omar", MALE),
+                new Person("John", MALE),
+                new Person("Alissa", FEMALE),
+                new Person("Maria", FEMALE),
+                new Person("Alice", FEMALE),
+                new Person("Bob", PREFER_NOT_TO_SAY)
+        );
+
+        System.out.println("Generos:");
+
+        people.stream()
+                .map(person -> person.gender)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        System.out.println("Nombres:");
+
+        people.stream()
+                .map(person -> person.name)
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+
+        System.out.println("Largo nombres:");
+
+        people.stream()
+                .map(person -> person.name)
+                .mapToInt(String::length)
+                .forEach(System.out::println);
+
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        FEMALE,
+        MALE,
+        PREFER_NOT_TO_SAY
+    }
+
+}
